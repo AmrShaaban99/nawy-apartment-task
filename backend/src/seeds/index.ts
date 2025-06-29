3b28f0dc0ca040c99dc6fdc6f7b6fc9fb096e81b
Amr shaaban <62676022+AmrShaaban99@users.noreply.github.com>
2025-06-29 18:20:32 +0300

    feat: add area seeding to the global seed runner

--- backend/src/seeds/index.ts
+++ backend/src/seeds/index.ts
@@ -1,10 +1,12 @@
 // src/seeds/index.ts
 import { seedCities } from '../modules/city/city.seed';
 import { seedCountries } from '../modules/country/country.seed';
+import { seedAreas } from '../modules/area/area.seed';
 
 const runSeeds = async () => {
   await seedCountries();
   await seedCities();
+  await seedAreas();
 
   console.log('All seeds completed');
   process.exit(0);
